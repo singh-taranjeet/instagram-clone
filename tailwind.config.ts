793b4463d6d4f1449c1c0f9788d8c74d88b5d6cc
Taranjeet Singh <93113908+singh-taranjeet@users.noreply.github.com>
2024-06-06 17:56:18 +1000

    created nav and footer components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,34 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      spacing: {
+        gutter: "1rem",
+        small: "0.75rem", // => 12px
+        normal: "1.25rem", // > 20px
+        large: "2.5rem", // => 40px
+      },
+      colors: {
+        primary: "#FCCE02",
+        secondary: "#bfdbfe", // blue-200
+        light: "#eff6ff", // blue-100
+        "dark-blue": "#7e22ce",
+        "neon-blue": "#00FFFF",
+        "neon-green": "#7CFC00",
+        "neon-yellow": "#F4FF33",
+      },
+      padding: {
+        "rectangle-small": "0.5rem 1rem",
+        "rectangle-normal": "0.75rem 1.25rem",
+      },
+      fontSize: {
+        small: "1rem", // 16px
+        medium: "1.125rem", // => 20px
+        large: "1.875rem", // => 30px
+        "very-large": "3rem", // => 48px
+        "extra-very-large": "4.5rem", // => 72px
+      },
+      screens: {
+        xs: "420px",
       },
     },
   },
