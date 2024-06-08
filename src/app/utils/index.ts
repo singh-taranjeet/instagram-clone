d9f16d440beea1b8dbcbfd8526ed80c1a3e8fb1a
Taranjeet Singh <93113908+singh-taranjeet@users.noreply.github.com>
2024-06-08 23:04:51 +1000

    Update index.ts

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -4,11 +4,11 @@ import { cache } from "react";
 
 export const queries = {
   fetchUsers: () => {
-    return fetch("http://localhost:3000/api/users").then((res) => res.json());
+    return fetch(`${window.location.href}api/users`).then((res) => res.json());
   },
   fetchPosts: async (pageParam: any) => {
     const res = await fetch(
-      `http://localhost:3000/api/posts?page=${pageParam.pageParam}`
+      `${window.location.href}api/posts?page=${pageParam.pageParam}`
     ).then((r) => r.json());
     return res;
   },
