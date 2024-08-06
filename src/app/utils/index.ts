2438c512b0aff1311504738b3c87d89202dceef7
Taranjeet Singh <93113908+singh-taranjeet@users.noreply.github.com>
2024-08-06 05:18:20 -0700

    Build fix 3 (#7)

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -21,6 +21,7 @@ export const rootQueries = {
   fetchUsers: {
     name: "fetchUsers",
     queryFn: () => {
+      return [];
       return fetch(`${baseUrl}/users`).then((res) => res.json());
     },
   },
