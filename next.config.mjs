eaeeef62c5d643482604f52c894be0dd4ac41ca3
Taranjeet Singh <93113908+singh-taranjeet@users.noreply.github.com>
2024-08-20 11:06:25 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
   swcMinify: true, // Enable SWC minification for improved performance
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
