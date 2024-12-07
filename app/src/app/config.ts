24bfc55a798ba29c1c8924d0047b2c6fa2f08289
Taranjeet Singh <93113908+singh-taranjeet@users.noreply.github.com>
2024-12-07 18:54:01 +1100

    cdk configuration

--- app/src/app/config.ts
+++ app/src/app/config.ts
@@ -1,4 +1,4 @@
-const prodUrl = "insta-api.send-to-cloud.com";
+const prodUrl = "boloon-api.taranjeet-singh.com/";
 console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 export const CONFIG = {
   baseUrl:
